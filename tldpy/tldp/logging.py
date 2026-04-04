5a2351fdaf84bb181fab71fd77870d64ee7a58aa
ᎠᎡ. Ѕϵrgϵ Ѵictor <ser@users.noreply.github.com>
2026-04-04 06:26:35 +0000

    initial staage.

---
+++ tldpy/tldp/logging.py
@@ -0,0 +1,30 @@
+import logging
+import sys
+
+from loguru import logger
+
+class InterceptHandler(logging.Handler):
+    def emit(self, record):
+        # Get corresponding Loguru level if it exists
+        try:
+            level = logger.level(record.levelname).name
+        except ValueError:
+            level = record.levelno
+
+        # Find caller from where originated the logged message
+        frame, depth = logging.currentframe(), 2
+        while frame.f_code.co_filename == logging.__file__:
+            frame = frame.f_back
+            depth += 1
+
+        logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+def logging():
+    logger.remove()
+    logger.add(
+        sys.stdout,
+        format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+        level="INFO",
+        colorize=True
+    )
+    logging.basicConfig(handlers=[InterceptHandler()], level=0, force=True)
