739431b02f7b192bb0c41704e2697b80e90a185b
ᎠᎡ. Ѕϵrgϵ Ѵictor <ser@users.noreply.github.com>
2026-04-05 14:46:19 +0000

    Add TEMPLATES setting for admin

--- tldpy/tldp/settings.py
+++ tldpy/tldp/settings.py
@@ -106,6 +106,22 @@ STATIC_URL = "static/"
 
 ROOT_URLCONF = "tldp.urls"
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "tldp" / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
 # MeiliSearch
 MEILISEARCH = {
     "HOST": env("MEILI_URL", default="localhost"),
